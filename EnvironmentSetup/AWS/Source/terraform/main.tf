a77898f973850855e152e24ce570768969646e85
lpm0073 <lpm0073@gmail.com>
2023-07-04 12:13:14 -0600

    modify to run in existing vpc and eks cluster

--- EnvironmentSetup/AWS/Source/terraform/main.tf
+++ EnvironmentSetup/AWS/Source/terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-tfstate-${var.cluster-name}"
+    bucket         = "${account_id}-terraform-tfstate-${var.cluster-name}"
     key            = "global/s3/terraform.tfstate"
     region         = "${var.aws_region}"
     dynamodb_table = "${var.dynamodb_table}"
@@ -20,11 +20,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = data.aws_eks_cluster.cookiecutter.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = data.aws_eks_cluster.cookiecutter.cluster_id
 }
 
 provider "kubernetes" {
@@ -36,66 +36,66 @@ provider "kubernetes" {
 data "aws_availability_zones" "available" {
 }
 
-module "vpc" {
-  source                 = "terraform-aws-modules/vpc/aws"
-  version                = "3.14.2"
-  name                   = "${var.cluster-name}-vpc"
-  cidr                   = "10.168.0.0/16"
-  azs                    = data.aws_availability_zones.available.names
-  private_subnets        = ["10.168.128.0/18", "10.168.192.0/18"]
-  public_subnets         = ["10.168.0.0/18", "10.168.64.0/18"]
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  enable_dns_hostnames   = true
+###############################################################################
+#                                RESOURCES
+###############################################################################
+
+#------------------------------------------------------------------------------
+# swap out newly created vpc and eks for for our preexisting resources
+#------------------------------------------------------------------------------
+data "aws_vpc" "cookiecutter" {
+  filter = {
+    name = "Name",
+    value = "${var.cookiecutter_common_resource_name}"
+  }
+}
+
+data "aws_eks_cluster" "cookiecutter" {
+  name = "${var.cookiecutter_common_resource_name}"
+}
 
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
-    "kubernetes.io/role/elb"                    = "1"
+resource "kubernetes_namespace" "was" {
+  metadata {
+    name = "was"
   }
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
-    "kubernetes.io/role/internal-elb"           = "1"
-  }
+  depends_on = [data.data.aws_eks_cluster.cookiecutter]
+}
+
+# MCDANIEL NOTE: dedicated managed node group for WAF added to openedx_devops
+
+
+# module "eks" {
+#   source                    = "terraform-aws-modules/eks/aws"
+#   version                   = "16.1.0"
+#   cluster_name              = var.cluster-name
+#   cluster_version           = var.cluster-version
+#   subnets                   = data.aws_vpc.cookiecutter.private_subnets
+#   vpc_id                    = data.aws_vpc.cookiecutter.vpc_id
+#   write_kubeconfig          = false
+#   cluster_create_timeout    = "120m"
   
-  tags = {
-    Terraform = "true"
-    Environment = "${var.cluster-name}"
-  }
+#   tags = {
+#     Environment = "Wolfram Application Server"
+#   }
 
-}
+#   node_groups = {
+#     eks = {
+#       name             = "${var.cluster-name}-worker-nodes"
+#       desired_capacity = var.desired-worker-node
+#       max_capacity     = var.max-worker-node
+#       min_capacity     = var.min-worker-node
+#       disk_size        = var.disk-size
+#       instance_types    = [var.instance_type]
+#     }
+#   }
 
-module "eks" {
-  source                    = "terraform-aws-modules/eks/aws"
-  version                   = "16.1.0"
-  cluster_name              = var.cluster-name
-  cluster_version           = var.cluster-version
-  subnets                   = module.vpc.private_subnets
-  vpc_id                    = module.vpc.vpc_id
-  write_kubeconfig          = false
-  cluster_create_timeout    = "120m"
-  
-  tags = {
-    Environment = "Wolfram Application Server"
-  }
+#   workers_additional_policies = [aws_iam_policy.worker_policy.arn]
+# }
 
-  node_groups = {
-    eks = {
-      name             = "${var.cluster-name}-worker-nodes"
-      desired_capacity = var.desired-worker-node
-      max_capacity     = var.max-worker-node
-      min_capacity     = var.min-worker-node
-      disk_size        = var.disk-size
-      instance_types    = [var.instance_type]
-    }
-  }
+# resource "aws_iam_policy" "worker_policy" {
+#   name        = "node-workers-policy-${var.cluster-name}"
+#   description = "Node Workers IAM policies"
 
-  workers_additional_policies = [aws_iam_policy.worker_policy.arn]
-}
-
-resource "aws_iam_policy" "worker_policy" {
-  name        = "node-workers-policy-${var.cluster-name}"
-  description = "Node Workers IAM policies"
-
-  policy = file("${path.module}/iam-policy.json")
-}
+#   policy = file("${path.module}/iam-policy.json")
+# }
--- EnvironmentSetup/AWS/Source/terraform/variables.tf
+++ EnvironmentSetup/AWS/Source/terraform/variables.tf
@@ -1,13 +1,22 @@
+variable "account_id" {
+  default = "320713933456"
+}
+variable "cookiecutter_common_resource_name" {
+  defaults = "stepwisemath-global-live"
+} 
 variable "aws_region" {
-  default = "us-east-1"
+  #default = "us-east-1"
+  default = "us-east-2"
 }
 
 variable "cluster-name" {
-  default = "WAS"
+  #default = "WAS"
+  default = "stepwisemath-global-live"
 }
 
 variable "cluster-version" {
-  default = "1.24"
+  #default = "1.24"
+  default = "1.25"
 }
 
 variable "disk-size" {
