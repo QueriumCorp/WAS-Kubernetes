68b812152fa7483ace210ea3854c470ae9ac40b2
lpm0073 <lpm0073@gmail.com>
2023-07-05 20:19:30 -0600

    data aws_eks_cluster should not depend on module output

--- EnvironmentSetup/AWS/Source/terraform/main.tf
+++ EnvironmentSetup/AWS/Source/terraform/main.tf
@@ -26,11 +26,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  name = local.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name = local.cluster_name
 }
 
 provider "kubernetes" {
