71b66340b7c460583e820c204c28d1b3404250cb
lpm0073 <lpm0073@gmail.com>
2023-07-05 21:59:42 -0600

    more version constraints

---
+++ EnvironmentSetup/AWS/Source/terraform/providers.tf
@@ -0,0 +1,17 @@
+data "aws_eks_cluster" "eks" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
