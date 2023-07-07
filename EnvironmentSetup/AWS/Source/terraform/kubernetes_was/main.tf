9bd62e60a49216ddbb690e8de4924309bb73080d
lpm0073 <lpm0073@gmail.com>
2023-07-06 18:31:53 -0600

    move resource manager resources to was

--- EnvironmentSetup/AWS/Source/terraform/kubernetes_was/main.tf
+++ EnvironmentSetup/AWS/Source/terraform/kubernetes_was/main.tf
@@ -2,6 +2,10 @@ locals {
   cluster_name    = var.cluster_name
   aws_region      = var.aws_region
   account_id      = var.account_id
+
+  subnet_ids_list         = tolist(data.aws_subnets.was.ids)
+  subnet_ids_random_index = random_id.index.dec % length(data.aws_subnets.was.ids)
+  instance_subnet_id      = local.subnet_ids_list[local.subnet_ids_random_index]
 }
 terraform {
   backend "s3" {
@@ -20,3 +24,16 @@ terraform {
 data "aws_availability_zones" "available" {
 }
 
+data aws_subnets "was" {
+  filter {
+    name   = "vpc-id"
+    values = [ module.vpc.vpc_id ]
+  }
+}
+data "aws_subnet" "private_subnet" {
+  id = local.instance_subnet_id
+}
+
+resource random_id index {
+  byte_length = 2
+}
