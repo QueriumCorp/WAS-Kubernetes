97ba19a9ccd2a736f035129ef35f52ae38d6bbc2
lpm0073 <lpm0073@gmail.com>
2023-07-08 10:55:55 -0600

    move all k8s providers to main

--- EnvironmentSetup/AWS/Source/terraform/main.tf
+++ EnvironmentSetup/AWS/Source/terraform/main.tf
@@ -1,9 +1,4 @@
 # need this bc the default aws profile specifies us-east-2
-provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
-}
-
 
 module "vpc" {
     source = "./modules/vpc"
