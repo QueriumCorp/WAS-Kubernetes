8465ac8256b442f093e10c79e77392299f839c82
lpm0073 <lpm0073@gmail.com>
2023-07-11 08:11:59 -0600

    revert to helm

--- EnvironmentSetup/AWS/terraform/modules/kubernetes_kafka_strimzi/main.tf
+++ EnvironmentSetup/AWS/terraform/modules/kubernetes_kafka_strimzi/main.tf
@@ -4,42 +4,54 @@
 #
 # date: Aug-2022
 #
-# usage:  installs strimzi operator for kafka.
-# see:    https://strimzi.io/quickstarts/
+# usage: installs strimzi operator for kafka.
+# see: https://artifacthub.io/packages/helm/strimzi/strimzi-kafka-operator
 #
+# requirements: you must initialize a local helm repo in order to run
+# this mdoule.
+#
+#   brew install helm
+#   helm repo add strimzi https://strimzi.io/charts/
+#   helm repo update
+#   helm search repo strimzi
+#   helm show values strimzi
+
+# NOTE: run `helm repo update` prior to running this
+#       Terraform module.
 #-----------------------------------------------------------
 locals {
   kafka_namespace = "kafka"
+  tags = {}
 }
 
-data "http" "strimzi" {
-  url = "https://strimzi.io/install/latest?namespace=kafka"
+data "template_file" "strimzi-values" {
+  template = file("${path.module}/yml/strimzi-values.yaml")
 }
 
-data "template_file" "strimzi" {
-  template = file("${path.module}/yml/strimzi.yaml")
-  vars = {
-    name = var.name
-  }
-}
 data "template_file" "kafka" {
   template = file("${path.module}/yml/kafka-persistent.yaml.tpl")
   vars = {
     name = var.name
   }
 }
-resource "kubernetes_namespace" "kafka" {
-  metadata {
-    name = local.kafka_namespace
-  }
-}
 
-resource "kubectl_manifest" "strimzi" {
-  yaml_body  = data.template_file.strimzi.rendered
-  depends_on = [ kubernetes_namespace.kafka ]
+
+resource "helm_release" "strimzi" {
+  namespace        = local.kafka_namespace
+  create_namespace = true
+
+  name       = "strimzi"
+  repository = "https://strimzi.io/charts/"
+  chart      = "strimzi-kafka-operator"
+  version    = "~> 0.35"
+
+  values = [
+    data.template_file.strimzi-values.rendered
+  ]
+
 }
 
 resource "kubectl_manifest" "kafka" {
   yaml_body  = data.template_file.kafka.rendered
-  depends_on = [kubectl_manifest.strimzi]
+  depends_on = [helm_release.strimzi]
 }
