8e3af1bdf63e04047f00493f9a14f526a9738171
lpm0073 <lpm0073@gmail.com>
2023-07-12 13:57:08 -0600

    one topic per manifest

--- EnvironmentSetup/AWS/terraform/modules/kubernetes_was/manifests.tf
+++ EnvironmentSetup/AWS/terraform/modules/kubernetes_was/manifests.tf
@@ -73,8 +73,24 @@ data "template_file" "hpa-autoscaler-endpoint-manager" {
   }
 }
 
-data "template_file" "was-topics" {
-  template = file("${path.module}/yml/was-topics.yaml.tpl")
+data "template_file" "was-topic-endpoint-info" {
+  template = file("${path.module}/yml/was-topic-endpoint-info.yaml.tpl")
+  vars = {
+    name            = var.shared_resource_name
+    kafka_namespace = "kafka"
+  }
+}
+
+data "template_file" "was-topic-nodefile-info" {
+  template = file("${path.module}/yml/was-topic-nodefile-info.yaml.tpl")
+  vars = {
+    name            = var.shared_resource_name
+    kafka_namespace = "kafka"
+  }
+}
+
+data "template_file" "was-topic-resource-info" {
+  template = file("${path.module}/yml/was-topic-resource-info.yaml.tpl")
   vars = {
     name            = var.shared_resource_name
     kafka_namespace = "kafka"
@@ -84,9 +100,14 @@ data "template_file" "was-topics" {
 #------------------------------------------------------------------------------
 #                             kafka
 #------------------------------------------------------------------------------
-resource "kubectl_manifest" "was-topics" {
-  yaml_body = data.template_file.was-topics.rendered
-
+resource "kubectl_manifest" "was-topic-endpoint-info" {
+  yaml_body = data.template_file.was-topic-endpoint-info.rendered
+}
+resource "kubectl_manifest" "was-topic-nodefile-info" {
+  yaml_body = data.template_file.was-topic-nodefile-info.rendered
+}
+resource "kubectl_manifest" "was-topic-resource-info" {
+  yaml_body = data.template_file.was-topic-resource-info.rendered
 }
 
 
@@ -113,14 +134,18 @@ resource "kubectl_manifest" "service-resource-manager" {
 resource "kubectl_manifest" "deployment-resource-manager" {
   yaml_body = data.template_file.deployment-resource-manager.rendered
   depends_on = [
-    kubectl_manifest.was-topics,
+    kubectl_manifest.was-topic-endpoint-info,
+    kubectl_manifest.was-topic-nodefile-info,
+    kubectl_manifest.was-topic-resource-info,
     kubectl_manifest.service-endpoint-manager
   ]
 }
 resource "kubectl_manifest" "deployment-endpoint-manager" {
   yaml_body = data.template_file.deployment-endpoint-manager.rendered
   depends_on = [
-    kubectl_manifest.was-topics,
+    kubectl_manifest.was-topic-endpoint-info,
+    kubectl_manifest.was-topic-nodefile-info,
+    kubectl_manifest.was-topic-resource-info,
     kubectl_manifest.service-endpoint-manager
   ]
 }
@@ -128,7 +153,9 @@ resource "kubectl_manifest" "deployment-endpoint-manager" {
 resource "kubectl_manifest" "deployment-active-web-elements-server" {
   yaml_body = data.template_file.deployment-active-web-elements-server.rendered
   depends_on = [
-    kubectl_manifest.was-topics,
+    kubectl_manifest.was-topic-endpoint-info,
+    kubectl_manifest.was-topic-nodefile-info,
+    kubectl_manifest.was-topic-resource-info,
     kubectl_manifest.service-active-web-elements-server,
     kubectl_manifest.deployment-resource-manager,
     kubectl_manifest.deployment-endpoint-manager
